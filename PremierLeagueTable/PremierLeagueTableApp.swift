bf8c090d54fd8be2c888f7f302cf9a74c4b08285
Matthew O'Leary <moleary@zendesk.com>
2022-07-28 13:33:16 +0100

    Initial Commit

---
+++ PremierLeagueTable/PremierLeagueTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueTableApp.swift
+//  PremierLeagueTable
+//
+//  Created by Matthew O'Leary on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
